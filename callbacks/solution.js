4b197bc63ed77d9cfedc067f82fc8a1e60dde82b
Antonio Mata <antonio.mata.sv@gmail.com>
2022-04-10 13:33:38 -0600

    improve instructions

--- callbacks/solution.js
+++ callbacks/solution.js
@@ -1,7 +1,7 @@
 /*
 INSTRUCTIONS
 
-1. create an array that contains 5 names, include 1, or more, of the allowed usernames located in validate-user.js
+1. create an array that contains 5 names, include 1 or more of the allowed usernames located in validate-user.js
 2. iterate the array, keep an eye on performance, and validate every username with the function exported in validate-user.js
 3. process and format every result, so that the program console.log the success results in a group, and the failure results in a group
 
@@ -23,10 +23,24 @@ User Benjamin not allowed
 4. if you want to challenge yourself, add the needed logic so the program can read the array of names from the terminal
 ** check about node.js process.argv **
 
+Example:
+
 node solution.js name1,name2,name3, or
 node solution.js name1 name2 name3
 
-YOUR SOLUTION GOES AFTER THIS
+5. another challenge is: after you solve the challenge using callback style, in another file promisify the callback and solve it again
 */
+
+function solution() {
+    // YOUR SOLUTION GOES HERE
+
+    // you get your 5 names here
+
+    // iterate the names array and validate them with the method
+
+    // log the final result
+}
+
+solution()
 
 
