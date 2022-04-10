3e64983e1c346ff88c68768616cac020cbd506dc
Antonio Mata <antonio.mata.sv@gmail.com>
2022-04-09 20:41:37 -0600

    add callback solution instructions

---
+++ callbacks/solution.js
@@ -0,0 +1,32 @@
+/*
+INSTRUCTIONS
+
+1. create an array that contains 5 names, include 1, or more, of the allowed usernames located in validate-user.js
+2. iterate the array, keep an eye on performance, and validate every username with the function exported in validate-user.js
+3. process and format every result, so that the program console.log the success results in a group, and the failure results in a group
+
+Example:
+
+Success
+
+id:1
+name: John
+
+id:2
+name: Mary
+
+Failure
+
+User Michael not allowed
+User Benjamin not allowed
+
+4. if you want to challenge yourself, add the needed logic so the program can read the array of names from the terminal
+** check about node.js process.argv **
+
+node solution.js name1,name2,name3, or
+node solution.js name1 name2 name3
+
+YOUR SOLUTION GOES AFTER THIS
+*/
+
+
--- callbacks/validate-user.js
+++ callbacks/validate-user.js
@@ -4,12 +4,11 @@ const allowedUsers = ['John', 'Mary', 'Richard', 'Stacy']
 
 module.exports = (name, cb) => {
     if (!allowedUsers.includes(name)) {
-        cb(Error('User not allowed'))
+        cb(Error(`User ${name} not allowed`))
         return
     }
 
     setTimeout(() => {
-        // const id = Math.random().toString(36).split('.')[1].slice(0, 4)
         const id = Math.floor(Math.random() * 101)
         cb(null, {id, name})
     }, 300)
