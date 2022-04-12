cc68be56c9c685f6c85818eb7e5e3a6ed978fcf7
Jose Tobar <josectobar@gmail.com>
2022-04-12 10:44:31 -0700

    revert promise

--- 1-callbacks/validate-user.js
+++ 1-callbacks/validate-user.js
@@ -2,14 +2,14 @@
 
 const allowedUsers = ["John", "Mary", "Richard", "Stacy"];
 
-module.exports = (name, cb) =>
-  new Promise(function (resolve) {
-    if (!allowedUsers.includes(name)) {
-      return resolve(cb(Error(`User ${name} not allowed`)));
-    }
+module.exports = (name, cb) => {
+  if (!allowedUsers.includes(name)) {
+    cb(Error(`User ${name} not allowed`));
+    return;
+  }
 
-    setTimeout(() => {
-      const id = Math.floor(Math.random() * 101);
-      resolve(cb(null, { id, name }));
-    }, 300);
-  });
+  setTimeout(() => {
+    const id = Math.floor(Math.random() * 101);
+    cb(null, { id, name });
+  }, 300);
+};
