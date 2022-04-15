0ed465828d46090eb7205cba51e0c19a9c635391
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-04-15 12:52:17 -0600

    chore: add solution to base challenge

--- 3-async-await/solution.js
+++ 3-async-await/solution.js
@@ -22,15 +22,72 @@ the format is up to you
 */
 
 function solution() {
-    // YOUR SOLUTION GOES HERE
-
+    const getProduct = require('./products');
+    const getPrice = require('./prices');
+    
     // You generate your id value here
+    const id = Date.now().toString().slice(-2);
+    const detail = { id };
+    let errorMsg = '';
 
     // You use Promise.all here
+    const promiseAll = Promise.all([getProduct(id), getPrice(id)])
+        .then((data) => {
+            detail.product = data[0];
+            detail.price = data[1];
+        })
+        .catch((err) => {
+            errorMsg = err.message;
+        });
 
     // You use Promise.allSettled here
+    const promiseAllSettled = Promise.allSettled([getProduct(id), getPrice(id)])
+        .then((values) => {
+            let results = values; // CR: Is this variable necessary?
+            results.map((result) => {
+                if (result.status === 'fulfilled' && typeof result.value === 'string') {
+                    detail.product = result.value;
+                }
+                if (result.status === 'fulfilled' && typeof result.value === 'number') {
+                    detail.price = result.value;
+                }
+                if (result.status === 'rejected') {
+                    errorMsg = result.reason.message;
+                }
+            })
+        }) 
+        .catch((err) => errorMsg = err.message);
 
     // Log the results or errors here
+    async function asyncPromAll() {
+        try {
+            await promiseAll;
+            if (detail.hasOwnProperty('product') && detail.hasOwnProperty('price')) {
+                console.log('I come from asyncPromAll:', detail);
+            } else {
+                console.log('I come from asyncPromAll:', errorMsg);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }   
+    }
+
+    async function asyncPromAllSettled() {
+        try {
+            await promiseAllSettled;
+            if (detail.hasOwnProperty('product') && detail.hasOwnProperty('price')) {
+                console.log('I come from asyncPromAllSettled:', detail);
+            } else {
+                console.log('I come from asyncPromAllSettled:', errorMsg);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }   
+    }
+
+    asyncPromAll();
+    asyncPromAllSettled();
+    
 }
 
 solution()
