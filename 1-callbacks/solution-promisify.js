b6a010ea4a68624adb9ed00d94cce12d85e4a9ed
Diana Calderon <dianacalderon@ravn.co>
2022-07-17 19:27:04 -0600

    Getting names from enviroment variables and initializing variables

--- 1-callbacks/solution-promisify.js
+++ 1-callbacks/solution-promisify.js
@@ -3,7 +3,17 @@ const validator = require('./validate-user');
 
 function solution() {
     // YOUR SOLUTION GOES HERE
-  
+    const vars = process.argv.slice(2).length ? [...process.argv.slice(2)] : ['John', 'Diana', 'Jose', 'Stacy', 'Mary'];
+    let splitNames = [];
+    if (vars[0]?.includes(',')) {
+      splitNames = vars[0].split(',');
+    }
+    const names = splitNames.length ? [...splitNames] : [...vars];
+
+    const failures = [];
+    const success = [];
+
+    let counter = 0;
 }
 
 solution();
