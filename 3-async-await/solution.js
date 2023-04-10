0d095e386b4dbbdbb402a746a5a8c28f6a705cce
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-10 11:09:05 -0600

    feat(add): Async/Await challenge

--- 3-async-await/solution.js
+++ 3-async-await/solution.js
@@ -1,3 +1,5 @@
+const prices = require('./prices')
+const products = require('./products')
 /*
 INSTRUCTIONS
 
@@ -20,16 +22,63 @@ Example:
 9. as extra challenge: add Promise.race() and Promise.any(), and try to get the idea of what happens
 */
 
-function solution() {
+async function solution() {
     // YOUR SOLUTION GOES HERE
 
     // You generate your id value here
+    const id = Date.now() % 100
 
+    const promisesArray = [prices(id), products(id)]
+    const allSettleResults = []
+    const promiseAllResults = []
+    const errors = []
     // You use Promise.all() here
+    try {
+        const promisesAll = await Promise.all(promisesArray)
+        promisesAll.forEach(res => promiseAllResults.push(res))
+    } catch (error) {
+        errors.push(`Promise all error ${error.message}`);
+    }
 
     // You use Promise.allSettled() here
+    try {
+        const promisesAllSettled = await Promise.allSettled(promisesArray)
 
+        promisesAllSettled.forEach(({ value, status }) => {
+            if (status === 'fulfilled') {
+                allSettleResults.push(value)
+            } else {
+                throw new Error()
+            }
+        })
+
+    } catch (error) {
+        errors.push(`Promise settle all error ${error.message}`);
+    }
     // Log the results, or errors, here
+    if (errors.length > 0) {
+        console.group('Errors')
+        errors.forEach(err => console.log(err))
+        console.groupEnd('Errors')
+    }
+    else {
+        console.group('Promise All')
+        console.log({
+            id,
+            product: promiseAllResults[1],
+            price: promiseAllResults[0],
+        });
+        console.groupEnd('Promise All')
+        console.log('\n');
+        console.group('Promise All Settled')
+        console.log({
+            id,
+            products: typeof allSettleResults[0] === 'string' ? allSettleResults[0] : allSettleResults[1],
+            price: typeof allSettleResults[0] === 'number' ? allSettleResults[0] : allSettleResults[1],
+        });
+        console.groupEnd('Promise All Settled')
+    }
+
 }
 
 solution()
