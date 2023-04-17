a155035c85f1d9269419b3dacb132610269dcdc7
Fabio Flores <fabioflores@ravn.co>
2023-04-17 10:44:20 -0600

    feat: add solution challenge callback

--- 1-callbacks/solution.js
+++ 1-callbacks/solution.js
@@ -2,7 +2,7 @@
 INSTRUCTIONS
 
 1. create an array that contains 5 names, include 1 or more of the allowed usernames located in validate-user.js
-2. iterate the array, keep an eye on performance, and validate every username with the function exported in validate-user.js
+2. iterate the array, keep an eye on performance, afirstnd validate every username with the function exported in validate-user.js
 3. process and format every result, so that the program console.log the success results in a group, and the failure results in a group
 
 Example:
@@ -31,17 +31,27 @@ node solution.js name1 name2 name3
 5. another challenge is: after you solve the challenge using callback style, in another file promisify the callback and solve it again
 ** give a look to node.js util.promisify, avoid to alter the validate-user.file **
 */
+const validateUser = require('./validate-user');
 
 function solution() {
-    // YOUR SOLUTION GOES HERE
+  // you get your 5 names here
+  const names = ['Melani', 'John', 'Mary', 'Fabio', 'Leidy'];
+  let validUsers = 'Success\n',
+    invalidUsers = '\nFailure\n\n',
+    counter = 0;
 
-    // you get your 5 names here
+  const callback = function (err, user) {
+    if (err) invalidUsers += err.message + '\n';
+    else validUsers += `\nid:${user.id}\nname:${user.name}\n`;
+    counter++;
+    if (counter === names.length) console.log(validUsers, invalidUsers);
+  };
 
-    // iterate the names array and validate them with the method
-
-    // log the final result
+  // iterate the names array and validate them with the method
+  names.forEach((name) => {
+    validateUser(name, callback);
+  });
+  // log the final result
 }
 
-solution()
-
-
+solution();
